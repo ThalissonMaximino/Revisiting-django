a8e4431631a6c3b4d144f4012c8baca221a5f0a2
ThalissonMaximino <thalissonmaximino@gmail.com>
2024-07-25 17:15:15 -0300

    add:templates

--- djangoproject/blog/views.py
+++ djangoproject/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = []
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request,'blog/about.html' )
