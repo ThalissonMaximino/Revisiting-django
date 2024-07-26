709ff2da19e74055f55084f4bf0d83f866eed102
ThalissonMaximino <thalissonmaximino@gmail.com>
2024-07-26 09:59:13 -0300

    add:base.html

--- djangoproject/blog/views.py
+++ djangoproject/blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
-posts = []
+posts = [
+    {
+        'author': 'thalisson',
+        'title': 'blog post 1',
+        'content': 'first post blog',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'janet',
+        'title': 'blog post 2',
+        'content': 'second post blog',
+        'date_posted': 'August 27, 2018'
+        },
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request,'blog/about.html' )
+    return render(request,'blog/about.html', {'title': 'About'} )
